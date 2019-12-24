1486fa3b77fb09ce65ec3b5fa14be3b81de6152b
Filip Chamier-Ciemiński <45480039+fcieminski@users.noreply.github.com>
2019-12-24 13:54:52 +0100

    change size text

--- src/Main.vue
+++ src/Main.vue
@@ -1,6 +1,21 @@
 <template>
   <main class="main__image">
-    <main-header />
+    <main-header @textSize="textSize" />
+    <section class="main__section">
+      <div class="section__box">
+        <h2 :class="`text_${fontSize} mb-8`">Lorem isum dolor sit amet</h2>
+        <div :class="`text_${fontSize} section__text`">
+          consectetur adipisicing elit. Sequi accusantium architecto esse
+          ducimus, autem harum quidem beatae nisi qui illo error aperiam rem
+          nostrum, vel impedit dolor sint aliquid modi.Eaque dolore quia
+          quibusdam autem suscipit excepturi consectetur, cum magnam laborum
+          exercitationem quam labore odit veritatis placeat sint mollitia vel
+          tempora error sit nemo earum! Voluptatum, reiciendis. Enim,
+          consequuntur perferendis?
+        </div>
+        <button :class="`text_${fontSize} section__button mt-8`">Lorem ipsum dolor</button>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -9,11 +24,59 @@ import MainHeader from "./components/Header";
 export default {
   components: {
     MainHeader
+  },
+
+  data: () => ({
+    fontSize: "medium"
+  }),
+
+  methods: {
+    textSize(value) {
+      if (value) {
+        this.fontSize = value === 1 ? "medium" : "big";
+      } else {
+        this.fontSize = "small";
+      }
+    }
   }
 };
 </script>
 
 <style lang="scss" scope>
+.main__section {
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  justify-content: center;
+  .section__box {
+    width: 50vw;
+    margin-left: -100px;
+    h2 {
+      line-height: 0.7;
+    }
+    .text_small {
+      font-size: 0.9rem;
+    }
+    .text_medium {
+      font-size: 1.1rem;
+    }
+    .text_big {
+      font-size: 1.5rem;
+    }
+  }
+  .section__text {
+    text-align: justify;
+  }
+  .section__button {
+    background-color: transparent;
+    border: 1px solid white;
+    color: white;
+    height: 35px;
+    width: 250px;
+  }
+}
 .main__image {
   width: 100vw;
   height: 100vh;
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,49 @@
 <template>
-  <header>
+  <header class="header">
     <div class="header__box">
-      <div>
-        <div>Martylologia</div>
-        <div>Wsi polskich</div>
+      <div class="box__title">
+        <div class="mb-2">Martylologia</div>
+        <div class="mt-2">Wsi polskich</div>
       </div>
       <div class="box__buttons">
         <div class="mr-6 d-flex">
-          <button class="buttons__header buttons__header--active">pl</button>
-          <button class="buttons__header">eng</button>
+          <button
+            @click="language = 0"
+            :class="[
+              language === 0 && 'buttons__header--active',
+              'buttons__header mr-2'
+            ]"
+          >pl</button>
+          <button
+            @click="language = 1"
+            :class="[
+              language === 1 && 'buttons__header--active',
+              'buttons__header'
+            ]"
+          >eng</button>
         </div>
         <div class="d-flex">
-          <button class="buttons__header buttons__lang--small buttons__header--active">A</button>
-          <button class="buttons__header buttons__lang--medium">A</button>
-          <button class="buttons__header buttons__lang--big">A</button>
+          <button
+            @click="changeTextSize(0)"
+            :class="[
+              textSize === 0 && 'buttons__header--active',
+              'buttons__header mr-2 buttons__lang--small'
+            ]"
+          >A</button>
+          <button
+            @click="changeTextSize(1)"
+            :class="[
+              textSize === 1 && 'buttons__header--active',
+              'buttons__header mr-2 buttons__lang--medium'
+            ]"
+          >A</button>
+          <button
+            @click="changeTextSize(2)"
+            :class="[
+              textSize === 2 && 'buttons__header--active',
+              'buttons__header buttons__lang--big'
+            ]"
+          >A</button>
         </div>
       </div>
     </div>
@@ -24,21 +54,36 @@
 export default {
   name: "MainHeader",
 
-  data: () => ({}),
+  data: () => ({
+    language: 0,
+    textSize: 1
+  }),
 
   computed: {},
 
-  methods: {}
+  methods: {
+    changeTextSize(size) {
+      this.textSize = size;
+      this.$emit("textSize", size);
+    }
+  }
 };
 </script>
 
 <style lang="scss" scope>
+.header {
+  position: fixed;
+  width: 100%;
+}
 .header__box {
   margin: 16px;
   align-items: center;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
+  .box__title {
+    color: white;
+  }
   .box__buttons {
     display: flex;
     .buttons__header {
