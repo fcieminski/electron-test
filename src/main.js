143be05bfa0db6f08a9ae1deb1e1cbcf12571f2a
Filip Chamier-Ciemiński <45480039+fcieminski@users.noreply.github.com>
2019-12-24 11:51:13 +0100

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 import Vue from "vue";
-import App from "./App.vue";
+import Main from "./Main.vue";
 import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(Main)
 }).$mount("#app");
